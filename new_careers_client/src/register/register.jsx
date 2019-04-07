bddcd96866770c28f8aa792bea923d39510fb714
grahamwren <graham@maineisland.co>
2019-04-07 09:39:46 -0400

    Register callback execution should be deferred

--- new_careers_client/src/register/register.jsx
+++ new_careers_client/src/register/register.jsx
@@ -1,20 +1,22 @@
-import React, {PureComponent} from 'react';
-import {SubmissionError} from 'redux-form';
-import {RegisterContainer} from './theme';
+import React, { PureComponent } from 'react';
+import { SubmissionError } from 'redux-form';
+import { RegisterContainer } from './theme';
 import RegisterForm from './register-form';
 import api from '../api';
 
 export default class Register extends PureComponent {
-  registerUser({name, email, password, confirmPassword}) {
+  registerUser({
+    name, email, password, confirmPassword
+  }) {
     const { history } = this.props;
     if (password !== confirmPassword) {
       throw new SubmissionError({
         email: 'Passwords do not match',
         _error: 'Register failed'
-      })
+      });
     }
-    const cb = history.push('/');
-    api.createUser({email, password, name}).then(cb, (error) => {
+    const cb = () => history.push('/');
+    api.createUser({ email, password, name }).then(cb, (error) => {
       if (error.status === 422) {
         throw new SubmissionError({
           email: 'Email already taken',
@@ -28,7 +30,7 @@ export default class Register extends PureComponent {
   render() {
     return (
       <RegisterContainer>
-        <RegisterForm  onSubmit={data => this.registerUser(data)}/>
+        <RegisterForm onSubmit={data => this.registerUser(data)} />
       </RegisterContainer>
     );
   }
